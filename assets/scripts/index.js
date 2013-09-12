d639ea9a1d0968c430305e20b9b1e38814e21d3c
Nat Hamilton <info@nathamilton>
2013-09-12 15:54:20 -0400

    Design added, Mobile playback in works

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,6 +14,7 @@
 		var video = document.getElementById('v1');
 		var video2 = document.getElementById('v2');
 		var video3 = document.getElementById('v3');
+		//var phone_v = document.getElementById('phone_v');
 		var audio = document.getElementById('a');
 
 		//usernames
@@ -26,6 +27,7 @@
 		var timer;
 
 		//play and close
+		var vInfo = $(".video-info")
 		var cover = $("#play_img");
 		var end = $("#end_img");
 		var player1 = $("#video_player1");
@@ -87,129 +89,262 @@
 		  	}
 		  }
 
-		video.setAttribute("src", insta_vid_list[videoIndex].url);
-  		video.load();
-  		video.muted = true;
-  		video2.setAttribute("src", insta_vid_list[videoIndex+1].url);
-  		video.load();
-  		video.muted = true;
-  		video3.setAttribute("src", "../images/ja_clips/jason_full_vid.mp4");
-  		video3.load();
-  		//video3.muted = true;
-  		/*audio.setAttribute("src", "../music/ja-nt.mp3");
-	    audio.load();*/
+			video.setAttribute("src", insta_vid_list[videoIndex].url);
+	  		video.load();
+	  		video.muted = true;
+	  		video2.setAttribute("src", insta_vid_list[videoIndex+1].url);
+	  		video.load();
+	  		video.muted = true;
+	  		video3.setAttribute("src", "../images/ja_clips/jason_full_vid.mp4");
+	  		video3.load();
 
-	    start.addEventListener("click", function() {
-      		if (clickCount % 2 == 0) {
-      			cover.addClass('closed');
-      			if (player3.hasClass('closed')&&player2.hasClass('closed')) {
-      				video.play();
-      				names.innerHTML = insta_vid_list[videoIndex].username;
-      			}else if(player3.hasClass('closed')&&player1.hasClass('closed')){
-      				video2.play();
-      				names.innerHTML = insta_vid_list[videoIndex].username;
-      			}else{
-      				video3.play();
-      				names.innerHTML = '';
-      			};
-      			video3.play();
-	      		timer = setTimeout(checkAudioTime, 1000);
-	      		clickCount++;
-      		}else{
-	      		if (player3.hasClass('closed')&&player2.hasClass('closed')) {
-      				video.pause();
-      			}else if(player3.hasClass('closed')&&player1.hasClass('closed')){
-      				video2.pause();
-      			}else{
-      				video3.pause();
-      			};
-      			clearTimeout(timer);
-      			video3.pause();
-	      		clickCount++;
-      		};
+		    start.addEventListener("click", function() {
+		    	vInfo.fadeTo(2000, 1, function(){
+		    		vInfo.delay(5000).fadeTo(2000, 0, function(){
+		    			vInfo.delay(10000).remove();
+		    		});
+		    	});
+		    	
+	      		if (clickCount % 2 == 0) {
+	      			cover.addClass('closed');
+	      			if (player3.hasClass('closed')&&player2.hasClass('closed')) {
+	      				video.play();
+	      				names.innerHTML = '<ul><li></li><li>'+insta_vid_list[videoIndex].username+'</li></ul>';
+	      			}else if(player3.hasClass('closed')&&player1.hasClass('closed')){
+	      				video2.play();
+	      				names.innerHTML = '<ul><li></li><li>'+insta_vid_list[videoIndex].username+'</li></ul>';
+	      			}else{
+	      				video3.play();
+	      				names.innerHTML = '';
 
-	    });
+	      			};
+	      			video3.play();
+		      		timer = setTimeout(checkAudioTime, 1000);
+		      		clickCount++;
+	      		}else{
+		      		if (player3.hasClass('closed')&&player2.hasClass('closed')) {
+	      				video.pause();
+	      			}else if(player3.hasClass('closed')&&player1.hasClass('closed')){
+	      				video2.pause();
+	      			}else{
+	      				video3.pause();
+	      			};
+	      			clearTimeout(timer);
+	      			video3.pause();
+		      		clickCount++;
+	      		};
 
-	  //audio time check
-	  var checkAudioTime = function(){
-	  	if(video3.currentTime.toFixed(0) == 220){
-	  		closing();
-	  	}else if(video3.currentTime.toFixed(0) == 215){
-	  		console.log('ending');
-	  		ending();
-	  		timer = setTimeout(checkAudioTime, 1000);
-	  	}else if(video3.currentTime.toFixed(0) == fanTimer && video3.currentTime.toFixed(0) != jaTimer){
-	  		fanTimer = fanTimer + 5;
-	  		fanVid();
-	  		timer = setTimeout(checkAudioTime, 1000);
-	  		console.log('fan');
-	  	}else if(video3.currentTime.toFixed(0) == jaTimer){
-	  		jaTimer = jaTimer + 25;
-	  		fanTimer = fanTimer + 10;
-	  		jaVid();
-	  		timer = setTimeout(checkAudioTime, 1000);;
-	  		console.log('jason');
-	  	}else{
-	  		timer = setTimeout(checkAudioTime, 1000);;
-	  		console.log(video3.currentTime.toFixed(0));
-	  	}
-	  }
+		    });
 
-	  //Jason video
-	  var jaVid = function(){	  	
-		player3.removeClass('closed');
-		player2.addClass('closed');
-		player1.addClass('closed');
-		setTimeout(function(){
-			  video.setAttribute("src", insta_vid_list[videoIndex+1].url);
-		  	  video.load();	
-		  }, 1000);
-		names.innerHTML = '';
-		jaCount++;
-	  }
+		  //audio time check
+		  var checkAudioTime = function(){
+		  	if(video3.currentTime.toFixed(0) == 230){
+		  		closing();
+		  	}else if(video3.currentTime.toFixed(0) == 225){
+		  		console.log('ending');
+		  		ending();
+		  		timer = setTimeout(checkAudioTime, 1000);
+		  	}else if(video3.currentTime.toFixed(0) == fanTimer && video3.currentTime.toFixed(0) != jaTimer){
+		  		fanTimer = fanTimer + 5;
+		  		fanVid();
+		  		timer = setTimeout(checkAudioTime, 1000);
+		  		console.log('fan');
+		  	}else if(video3.currentTime.toFixed(0) == jaTimer){
+		  		jaTimer = jaTimer + 25;
+		  		fanTimer = fanTimer + 10;
+		  		jaVid();
+		  		timer = setTimeout(checkAudioTime, 1000);
+		  		console.log('jason');
+		  	}else{
+		  		timer = setTimeout(checkAudioTime, 1000);
+		  		console.log(jaTimer);
+		  	}
+		  }
 
-	  //fan videos
-	  var fanVid = function(){
-	  	videoIndex++
-	  	if(insta_vid_list >= insta_vid_list.length) videoIndex = 0;
-	  	if(videoIndex%2==0){
-	  	  video.play();
-	  	  player1.removeClass('closed');
-	  	  player3.addClass('closed');
-		  player2.addClass('closed');
-		  setTimeout(function(){
-		  	video2.setAttribute("src", insta_vid_list[videoIndex+1].url);
-		  	video2.load();
-		  }, 1000);
-		  names.innerHTML = insta_vid_list[videoIndex].username;
-		  video.muted = true;
-		}else{
-		  video2.play();
-		  player2.removeClass('closed');
-		  player3.addClass('closed');
-		  player1.addClass('closed');
-		  setTimeout(function(){
-			  video.setAttribute("src", insta_vid_list[videoIndex+1].url);
-		  	  video.load();	
-		  }, 1000);
-		  names.innerHTML = insta_vid_list[videoIndex].username;
-		  video2.muted = true;
-		}
-		setTimeout(function(){
-		  //video3.pause();	
-		}, 500);
-	  }
+		  //Jason video
+		  var jaVid = function(){	  	
+			player3.removeClass('closed');
+			player2.addClass('closed');
+			player1.addClass('closed');
+			setTimeout(function(){
+				  video.setAttribute("src", insta_vid_list[videoIndex+1].url);
+			  	  video.load();	
+			  }, 1000);
+			names.innerHTML = '';
+			jaCount++;
+		  }
 
-	  var ending = function(){
-	  	jaTimer = 10000;
-	  	fanTimer = 10000;
-	  	jaVid();
-	  }
+		  //fan videos
+		  var fanVid = function(){
+		  	videoIndex++
+		  	if(insta_vid_list[videoIndex].url === undefined) videoIndex = 0;
+		  	if(insta_vid_list >= insta_vid_list.length) videoIndex = 0;
+		  	if(videoIndex%2==0){
+		  	  video.play();
+		  	  player1.removeClass('closed');
+		  	  player3.addClass('closed');
+			  player2.addClass('closed');
+			  setTimeout(function(){
+			  	video2.setAttribute("src", insta_vid_list[videoIndex+1].url);
+			  	video2.load();
+			  }, 1000);
+			  names.innerHTML = '<ul><li></li><li>'+insta_vid_list[videoIndex].username+'</li></ul>';
+			  video.muted = true;
+			}else{
+			  video2.play();
+			  player2.removeClass('closed');
+			  player3.addClass('closed');
+			  player1.addClass('closed');
+			  setTimeout(function(){
+				  video.setAttribute("src", insta_vid_list[videoIndex+1].url);
+			  	  video.load();	
+			  }, 1000);
+			  names.innerHTML = '<ul><li></li><li>'+insta_vid_list[videoIndex].username+'</li></ul>';
+			  video2.muted = true;
+			}
+		  }
 
-	  var closing = function(){
-	  	end.removeClass('closed');
-	  	cover.css({display: 'none'});
-	  	clearTimeout(timer);
-	  }
+		  var ending = function(){
+		  	jaTimer = 10000;
+		  	fanTimer = 10000;
+		  	jaVid();
+		  }
+
+		  var closing = function(){
+		  	end.removeClass('closed');
+		  	cover.css({display: 'none'});
+		  	clearTimeout(timer);
+		  	videoIndex = 0;
+			clipCount = 0;
+			jaTimer = 27;
+			fanTimer = 12;
+			clickCount = 0;
+		  	setTimeout(function(){
+		  		video.setAttribute("src", insta_vid_list[videoIndex].url);
+		  		video.load();
+		  		video.muted = true;
+		  		video2.setAttribute("src", insta_vid_list[videoIndex+1].url);
+		  		video.load();
+		  		video.muted = true;
+		  		video3.setAttribute("src", "../images/ja_clips/jason_full_vid.mp4");
+		  		video3.load();
+		  	}, 5000)
+		  }
+
+		  end.click(function(){
+		  	vInfo.fadeTo(2000, 1, function(){
+	    		vInfo.delay(1000).fadeTo(2000, 0, function(){
+	    			vInfo.delay(10000).css({display: 'none'});
+	    		});
+	    	});
+  			end.addClass('closed');
+  			video3.play();
+  			names.innerHTML = '';
+      		timer = setTimeout(checkAudioTime, 1000);
+      		clickCount++;
+      		cover.css({display: 'block'});
+		  });
+
+		  //Phone JS 
+			var phone_cover = document.getElementById("click");
+			var phone_click = $("#click");
+			console.log(phone_cover);
+
+			(function() {
+			    var context, 
+			        soundSource, 
+			        soundBuffer,
+			        url = '../music/ja-nt.mp3';
+
+			    // Step 1 - Initialise the Audio Context
+			    // There can be only one!
+			    function init() {
+			        if (typeof AudioContext !== "undefined") {
+			            context = new AudioContext();
+			        } else if (typeof webkitAudioContext !== "undefined") {
+			            context = new webkitAudioContext();
+			        } else {
+			            throw new Error('AudioContext not supported. :(');
+			        }
+			    }
+
+			    // Step 2: Load our Sound using XHR
+			    function startSound() {
+			    	alert('playing!')
+			        // Note: this loads asynchronously
+			        var request = new XMLHttpRequest();
+			        request.open("GET", url, true);
+			        request.responseType = "arraybuffer";
+
+			        // Our asynchronous callback
+			        request.onload = function() {
+			            var audioData = request.response;
+
+			            audioGraph(audioData);
+
+			        };
+
+			        request.send();
+			    }
+
+			    // Finally: tell the source when to start
+			    function playSound() {
+			        // play the source now
+			        soundSource.noteOn(context.currentTime);
+			    }
+
+			    function stopSound() {
+			        // stop the source now
+			        soundSource.noteOff(context.currentTime);
+			    }
+
+				phone_click.bind( "touchstart", startSound);
+
+			    // This is the code we are interested in
+			    function audioGraph(audioData) {
+			        // create a sound source
+			        soundSource = context.createBufferSource();
+
+			        // The Audio Context handles creating source buffers from raw binary
+			        soundBuffer = context.createBuffer(audioData, true/* make mono */);
+			      
+			        // Add the buffered data to our object
+			        soundSource.buffer = soundBuffer;
+
+			        // Plug the cable from one thing to the other
+			        soundSource.connect(context.destination);
+
+			        // Finally
+			        playSound(soundSource);
+			    }
+
+			    init();
+
+			}());
+
+			phone_v.setAttribute("src", insta_vid_list[videoIndex].url);
+	  		phone_v.load();
+	  		phone_v.muted = true;
+
+
+			function myNewSrc() {
+				if(videoIndex >= insta_vid_list.length) videoIndex = 0;
+				var phone_v = document.getElementsByTagName('video')[0];
+	            phone_v.src = insta_vid_list[videoIndex].url;
+	            phone_v.load();
+	            phone_v.muted = true;
+	            phone_v.play();				
+	        }
+	        
+	        function myAddListener(){
+	        	var phone_v = document.getElementsByTagName('video')[0];
+	        	phone_v.addEventListener("timeupdate", function() {
+		       		if (this.currentTime >= 5 || this.currentTime === this.duration) {
+		            	videoIndex++;
+		            	myNewSrc();
+		            	console.log(videoIndex);
+		        	}
+		    	}, false);
+	        }
 
 	});
